39f942fb9d6c2d4ef64288daa0bd9e1e6548a6da
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2020-02-25 18:10:09 -0500

    adding routes

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container } from "reactstrap";
+
+function Header() {
+  return (
+    <div>
+      <header>
+        <Container className="themed-container">
+          <nav></nav>
+        </Container>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
